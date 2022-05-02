432088b72047b3ecafed98439f9be89f6a7cc5ef
SilverAndrels <andresilva64@outlook.com>
2022-05-02 15:18:30 -0300

    ranges

---
+++ estruturasRepeticao/entendendo_ranges.py
@@ -0,0 +1,15 @@
+# Forma 1 por padrão inicia em 0 e o passo é de 1 em 1 até n -1 (n == numero de parada)
+for i in range(11):
+    print(i)
+
+# Forma 2 inicia em 1 e o passo é de 1 em 1 até n -1 (n == numero de parada)
+for i in range(1, 11):
+    print(i)
+
+# Forma 3 inicia em 1 e o passo é de 2 em 2 até n -1 (n == numero de parada)
+for i in range(1, 11, 2):
+    print(i)
+
+# Forma 4 inicia em 11 e vai de 1 em 1 até o 0 (zero)
+for i in range (11, -1, -1):
+    print(i)
