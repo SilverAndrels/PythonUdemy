989d5aeffb37cdb5a6941858f86317c9a5ef5b04
SilverAndrels <andresilva64@outlook.com>
2022-07-23 11:35:42 -0300

    add folder funcoes with exercises

---
+++ funcoes/definindo_funcoes.py
@@ -0,0 +1,33 @@
+'''
+Definindo Funções
+
+- Funções são pequenas partes de código que realizam tarefas específicas;
+- Pode ou não receber entrada de dados e retornar uma saída de dados;
+
+Funções  já utilizadas;
+- print()
+- len()
+- min()
+- max()
+- count()
+etc...
+
+# Exemplo utilizando funções
+
+colors = ['green', 'yellow', 'blue', 'white']
+
+# Utilizando a função integrada (Built-in) do python
+
+print(colors)
+
+colors.append('cyan blue')
+print(colors)
+'''
+# Como definir funções
+
+'''
+Forma geral de definir uma função
+
+def nome_da_funcao(parametros_de_entrada):
+    bloco_da_funcao
+'''
