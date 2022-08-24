17669f21b83f5c9a9c0983f27bb775eaa99835a9
SilverAndrels <andresilva64@outlook.com>
2022-08-24 19:55:27 -0300

    Docstrings

---
+++ funcoes/docstrings.py
@@ -0,0 +1,12 @@
+"""
+Documentando funções com Docstrings
+"""
+# Exemplo
+
+def diz_oi():
+    """Uma função simples que diz 'Oi !'"""
+    return 'Oi !'
+
+print(diz_oi())
+# Acessando o Docstrings da função diz_oi()
+print(diz_oi.__doc__)
