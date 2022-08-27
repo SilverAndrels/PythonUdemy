2027171f596a8135c474b43362514651537a287c
SilverAndrels <andresilva64@outlook.com>
2022-08-27 17:52:58 -0300

    Filter

---
+++ lambdass/filter.py
@@ -0,0 +1,14 @@
+"""
+Filter
+
+Filter serve para filtrar dados de uma determinada coleção.
+"""
+
+# Biblíoteca para trabalhar com dados estatísticos
+import statistics
+
+# Dados coletados de um sensor
+dados = [1.3, 2.7, 0.8, 4.1, 4.3, -0.1]
+
+# Utilizando mean() para calcular a média
+
