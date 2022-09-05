d2905b196e060a037c054d82559e7b1313d0aad8
SilverAndrels <andresilva64@outlook.com>
2022-09-05 11:14:43 -0300

    primeira aula tratamento de erros

---
+++ tratamento_de_erros/erros_mais_comuns.py
@@ -0,0 +1,29 @@
+"""
+Erros mais comuns
+
+SyntaxError -> Ocorre quando o python encontra um erro de síntaxe
+Ou seja, foi escrito algo que o python não reconhece como parte da linguagem.
+
+
+NameError -> Ocorre quando uma variável ou função não foi definida.
+
+
+TypeError -> Ocorre quando uma função/operação/ação é aplicada a um tipo errado.
+
+
+IndexError -> Ocorre ao tentar acessar um elemento em um dado indexável utilizando um
+índice inválido.
+
+
+ValueError -> Ocorre quando uma função/operação built-in (integrada ao Python)
+recebe um argumento com tipo correto mas valor inapropriado.
+
+
+KeyError -> Acontece quando tentamos acessar um dicionário com uma chave que não existe.
+
+
+AttributeError -> Ocorre quando uma variável não tem um atributo/função
+
+
+IdentationError -> Ocorre quando não respeitamos a identação do Python (4 espaços)
+"""
