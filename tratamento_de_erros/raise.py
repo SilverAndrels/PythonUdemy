bb8c6a64123fb19d8113e6d7239ea7799ff9d789
SilverAndrels <andresilva64@outlook.com>
2022-09-05 15:59:38 -0300

    tratamento de erros

---
+++ tratamento_de_erros/raise.py
@@ -0,0 +1,23 @@
+"""
+Levantando os própios erros com raise
+
+raise -> não é uma função. É uma palavra reservada como 'def' ou qualquer outra em Python.
+raise é útil para que criarmos nossas próprias exceções e mensagens de erro.
+
+Utilização geral é:
+raise TipoDoErro('mensasgem de erro')
+"""
+
+# Exemplo
+
+
+def colore(texto, cor):
+    if type(texto) is not str:
+        raise TypeError('texto precisa ser uma string')
+    if type(cor) is not str:
+        raise TypeError('cor precisa ser uma string')
+    print(f'O texto "{texto}" será impresso na cor {cor}')
+
+
+colore('André Silveira', 'Preto')
+
