89342b6b645dd9f9d4392564d7681bd128d022a9
SilverAndrels <andresilva64@outlook.com>
2022-09-08 13:34:56 -0300

    seek

--- leitura_e_escrita_de_arquivos/leitura_de_arquivos.py
+++ leitura_e_escrita_de_arquivos/leitura_de_arquivos.py
@@ -1,5 +1,7 @@
 """
 Leitura de arquivos
 
-
+para ler o conteúdo de um arquivo em Python: 
+utilizamos a função integrada open()
 """
+
---
+++ leitura_e_escrita_de_arquivos/seek_e_cursors.py
@@ -0,0 +1,29 @@
+"""
+seek() -> utilizada para movimentar o cursor pelo arquivo
+
+arquivo = open('txt.txt')
+print(arquivo.read())
+
+# move o cursor para o começo do arquivo.
+arquivo.seek(0)
+
+print(arquivo.read())
+
+# readline() -> lê o arquivo linha a linha
+
+a = arquivo.readline()
+print(a)
+
+# readlines()
+
+linhas = arquivo.readlines()
+
+print(len(linhas))
+
+"""
+# Abrindo o arquivo
+arquivo = open('txt.txt')
+
+# Para fechar o arquivo
+arquivo.close()
+
